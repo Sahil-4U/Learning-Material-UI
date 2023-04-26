4a42c0bb37e9a04e797f615f1ef0dba7f13ddebc
Sahil <ss6611786@gmail.com>
2023-04-26 10:25:06 +0530

    here i have added box property in react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 function App() {
   return (
     <div>
+      
       <center>
-       <Typography variant='h1'>Hi react</Typography>
+      <Box sx={{
+        p:2,
+        border:'1px dashed blue',
+        width:'600px',
+      }}>
+       <Typography variant='h4'>Hi react</Typography>
+       </Box>
       </center>
+      
     </div>
   )
 }
