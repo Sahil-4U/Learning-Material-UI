8dbfe1ebd09ae0a51b91e21ba80e09e8b1c39222
Sahil <ss6611786@gmail.com>
2023-05-29 20:56:50 +0530

    setup react project for simple interest app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+
 
 function App() {
+
+
   return (
     <div>
-      
-      <center>
-      <Box sx={{
-        p:2,
-        border:'1px dashed blue',
-        width:'600px',
-      }}>
-       <Typography variant='h4'>Hi react</Typography>
-       </Box>
-      </center>
-      
+
     </div>
   )
 }
