69b590c417deda1cb36d7807e081da8233baa3b3
Sahil <ss6611786@gmail.com>
2023-05-30 21:11:07 +0530

    face some error with style components

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,43 +1,45 @@
 import React from 'react'
-import { Badge, Divider, IconButton, Toolbar, Typography, makeStyles } from '@mui/material'
+import { Badge, Divider, IconButton, Toolbar, Typography } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import classes from "*.modules.css";
+import { styled } from "@mui/system";
 
-const useStyles=makeStyles((theme)=>({
+const useStyles=styled((theme)=>({
     title:{
-        flexGrow:1,
+       flexGap:1,
     },
-}))
-
-
+    tagline:{
+        fontSize:20,
+        justifyContent:'center',
+    }
+}));
 function Header() {
+   
+   const classes=useStyles();
 
-        const classes=useStyles();
-
-  return (
-   <>
-   <Toolbar>
-    <IconButton >
-        <MenuIcon/>
-    </IconButton >
-    <Typography variant='h6' className={classes.title}>Blogging Website</Typography>
-    <IconButton color='inherit'>
-    <Badge badgeContent={4} color="primary">
- <NotificationsIcon />
-</Badge>
-    </IconButton>
-    <IconButton color='inherit'>
-        <AccountCircleIcon/>
-    </IconButton>
-   </Toolbar>
-   <Divider />
-   <Toolbar className={classes.tagline}>
-    Express your fellings with words
-   </Toolbar>
-   </>
-  )
+    return (
+        <>
+            <Toolbar>
+                <IconButton >
+                    <MenuIcon />
+                </IconButton >
+                <Typography variant='h6' className={classes.title}>Blogging Website</Typography>
+                <IconButton color='inherit'>
+                    <Badge badgeContent={4} color="primary">
+                        <NotificationsIcon />
+                    </Badge>
+                </IconButton>
+                <IconButton color='inherit'>
+                    <AccountCircleIcon />
+                </IconButton>
+            </Toolbar>
+            <Divider />
+            <Toolbar className={classes.tagline}>
+                Express your fellings with words
+            </Toolbar>
+        </>
+    )
 }
 
 export default Header
